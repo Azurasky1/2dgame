65eb2e66d2de6ef5581d916f2aabd3f1fd3e6a02
Azurasky <tarutaruwhm@live.com>
2016-07-17 09:48:17 -0400

    Started moving over some of the previous in-game logic

---
+++ app/scripts/modules/healthbars.js
@@ -0,0 +1,15 @@
+(function () {
+// health bars
+
+this.width = width;
+this.height = height;
+this.color = hex;
+this.pos = {
+  x: anyPlayerOrEnemy.pos.x,
+  y: anyPlayerOrEnemy.pos.y - 20
+};
+
+
+
+
+})
